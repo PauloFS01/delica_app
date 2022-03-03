497043b2649b3cf007cc5bbda94890b2515237c0
Paulo Silva <paulosilva@Paulos-MacBook-Air.local>
2022-03-03 19:25:47 -0300

    config tab bar icons

--- src/navigation/tabs.js
+++ src/navigation/tabs.js
@@ -2,37 +2,50 @@ import * as React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
 
+import TabBarHome from "../components/svg/TabBarHome";
+import TabBarTrack from "../components/svg/TabBarTrack";
+import TabBarFavorites from "../components/svg/TabBarFavorites";
+import TabBarCategories from "../components/svg/TabBarCategories";
+import TabBarProfile from "../components/svg/TabBarProfile";
+
 import HomePage from "../screens/HomePage";
 import Categories from "../screens/Categories";
 import Favorites from "../screens/Favorites";
 import Profile from "../screens/Profile";
 
+import colors from "../themes/colors";
+
 const Tab = createBottomTabNavigator();
 
-const TabIcon = ({ route }) => ({
+const TabOptions = ({ route }) => ({
   tabBarIcon: ({ focused, color, size }) => {
     let iconName;
 
     if (route.name === "Home") {
-      iconName = focused ? "home-sharp" : "home-outline";
+      //   iconName = focused ? "home-sharp" : "home-outline";
+      return <TabBarHome size={size} color={color} />;
     } else if (route.name === "Categories") {
-      iconName = focused ? "albums-sharp" : "albums-outline";
+      //   iconName = focused ? "albums-sharp" : "albums-outline";
+      return <TabBarCategories size={size} color={color} />;
     } else if (route.name === "Favorites") {
-      iconName = focused ? "heart-sharp" : "heart-outline";
+      //   iconName = focused ? "heart-sharp" : "heart-outline";
+      return <TabBarFavorites size={size} color={color} />;
     } else if (route.name === "Profile") {
-      iconName = focused ? "person-sharp" : "person-outline";
+      //   iconName = focused ? "person-sharp" : "person-outline";
+      return <TabBarProfile size={size} color={color} />;
     }
 
     // You can return any component that you like here!
-    return <Ionicons name={iconName} size={size} color={color} />;
+    // return <TabBarHome size={size} color={color} />;
   },
-  tabBarActiveTintColor: "tomato",
-  tabBarInactiveTintColor: "gray",
+  tabBarActiveTintColor: `${colors.secundary}`,
+  tabBarInactiveTintColor: `${colors.textColor}`,
+  tabBarStyle: { backgroundColor: `${colors.inputBackground}` },
 });
 
 export default function Tabs() {
   return (
-    <Tab.Navigator screenOptions={TabIcon}>
+    <Tab.Navigator screenOptions={TabOptions}>
       <Tab.Screen name="Home" component={HomePage} />
       <Tab.Screen name="Favorites" component={Favorites} />
       <Tab.Screen name="Categories" component={Categories} />
